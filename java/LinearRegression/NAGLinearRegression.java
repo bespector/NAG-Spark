fda28132e376189a63de3a340cc95f5f96fef574
bspec3 <bspec3@gmail.com>
2015-02-02 00:39:28 -0600

    updated variable names

--- java/LinearRegression/NAGLinearRegression.java
+++ java/LinearRegression/NAGLinearRegression.java
@@ -26,8 +26,8 @@ public class NAGLinearRegression {
 
 	private int _numvars;
         private int _dataSize;
-        private int _chunkSize = 1000;
-        private double[] _CON ;
+        private int _chunkSize = 10000;
+        private double[] _con ;
         private double[] _coef;
         private double _Rsquared ;
         private int _ifail;
@@ -80,17 +80,17 @@ public class NAGLinearRegression {
                 }
                 
                 G02BU g02bu = new G02BU();
-                double[] MEANS = new double [numvars];
-                double[] SSQ = new double [(numvars*numvars+numvars)/2];
-                int IFAIL = 1;
+                double[] means = new double [numvars];
+                double[] ssq = new double [(numvars*numvars+numvars)/2];
+                int ifail = 1;
                 double sw = 0;
                 g02bu.eval("M", "U", length, numvars, x, length, x, 0.0, 
-                                                        MEANS, SSQ, IFAIL);
+                                                        means, ssq, ifail);
                 if(g02bu.getIFAIL()>0) {
                         System.out.println("Error with g02bu!!!");
                         System.exit(1);
                 }                        
-	      return Arrays.asList(new NAGData(length,MEANS,SSQ));
+	      return Arrays.asList(new NAGData(length,means,ssq));
 	    }
         }
 
@@ -112,23 +112,23 @@ public class NAGLinearRegression {
                 NAGData NAGEmpty = new NAGData(0, emptyMeans, emptySSQ);
                 NAGData finalpt = dataset.aggregate(NAGEmpty, new combineNAGData(),
                                                                 new combineNAGData());
-                int IFAIL = 1;
+                int ifail = 1;
                 int K1 = _numvars;
                 int K = K1 - 1;
 
-                double[] RESULT = new double[13];
-                double[] COEF = new double[K*3];
-                double[] CON = new double[3];
-                double[] RINV = new double[K*K];
-                double[] C = new double[K*K];
-                double[] WKZ = new double[K*K];
-                double[] SSQ;
+                double[] result = new double[13];
+                double[] coef = new double[K*3];
+                double[] con = new double[3];
+                double[] rinv = new double[K*K];
+                double[] c = new double[K*K];
+                double[] wkz = new double[K*K];
+                double[] ssq;
 
-                SSQ = convertMatrix(finalpt.ssq, K1);
+                ssq = convertMatrix(finalpt.ssq, K1);
 
                 G02BW g02bw = new G02BW();
 
-                g02bw.eval(_numvars, finalpt.ssq, IFAIL);
+                g02bw.eval(_numvars, finalpt.ssq, ifail);
                 if(g02bw.getIFAIL() > 0) {
                         System.out.println("Error with NAG (g02bw) IFAIL = " + g02bw.getIFAIL());       
                         System.exit(1);                
@@ -140,13 +140,13 @@ public class NAGLinearRegression {
                 _correlations = convertMatrix(finalpt.ssq, K1);
 
                 G02CG g02cg = new G02CG();                     
-                g02cg.eval(finalpt.length, K1, K, finalpt.means, SSQ, K1, pearsonR, 
-                                K1, RESULT, COEF, K, CON, RINV, K, C, K, WKZ, K, IFAIL);
+                g02cg.eval(finalpt.length, K1, K, finalpt.means, ssq, K1, pearsonR, 
+                                K1, result, coef, K, con, rinv, K, c, K, wkz, K, ifail);
 
                 long endTime = System.currentTimeMillis();
-                _CON = CON;
-                _Rsquared = RESULT[11];
-                _coef = COEF;
+                _con = con;
+                _Rsquared = result[11];
+                _coef = coef;
                 _ifail = g02cg.getIFAIL();
         }
 
@@ -158,8 +158,8 @@ public class NAGLinearRegression {
                 int N = n;
                 int LDA = N;
                 double[] B = new double[N*N];
-                int IFAIL = 1;
-                F01ZA f01za = new F01ZA(JOB, UPLO, DIAG, N, B, LDA, input, IFAIL);
+                int ifail = 1;
+                F01ZA f01za = new F01ZA(JOB, UPLO, DIAG, N, B, LDA, input, ifail);
                 f01za.eval();
                 if(f01za.getIFAIL()>0) {
                         System.out.println("Error with f01za!!!");
@@ -185,8 +185,8 @@ public class NAGLinearRegression {
                 bw.write("Total number of points: " + _dataSize + "\n");
                 bw.write("Var\t\t|Coef\t\tSE\t\tt-value\n");
                 bw.write("------------------------------------------------------\n");
-                bw.write(String.format("Intcp\t\t|%d\t\t%.1f\t\t%.1f", (int)_CON[0], 
-                                                        _CON[1], _CON[2]));
+                bw.write(String.format("Intcp\t\t|%d\t\t%.1f\t\t%.1f", (int)_con[0], 
+                                                        _con[1], _con[2]));
                 bw.newLine();
                 for(int i=0;i<_numvars-1;i++) 
                 {
